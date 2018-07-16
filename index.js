b1ab6682a94557436a0efc715aa18470e70b14ff
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-16 13:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holilday,name){
+function happyHolidayTo(holilday,name){
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday,days){
